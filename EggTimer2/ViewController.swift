9905d2335701f3a5176a44d0a9cbdfdc5ec3cfd3
Dilara Elçioğlu <delcioglu@gmail.com>
2022-08-26 18:00:53 +0300

    deleted all buttons

--- EggTimer2/ViewController.swift
+++ EggTimer2/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sizeButton(_ sender: Any) {
+        
+       
     }
     
 
