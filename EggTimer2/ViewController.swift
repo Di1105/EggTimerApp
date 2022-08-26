4a45d52a210e67fba2411c77dfb0ed3ab975862b
Dilara Elçioğlu <delcioglu@gmail.com>
2022-08-26 16:21:08 +0300

    delete all buttons

--- EggTimer2/ViewController.swift
+++ EggTimer2/ViewController.swift
@@ -8,34 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var softImage: UIImageView!
-    @IBOutlet weak var mediumImage: UIImageView!
-    
-    @IBOutlet weak var hardImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-    @IBAction func fridgeTemButton(_ sender: Any) {
-    }
-    @IBAction func roomTempbutton(_ sender: Any) {
-    }
-    
-    @IBAction func smallButton(_ sender: Any) {
-    }
-    
-    @IBAction func mediumButton(_ sender: Any) {
-    }
-    
-    @IBAction func largebutton(_ sender: Any) {
-    }
-    
-    
-    @IBAction func startCookButton(_ sender: Any) {
-    }
     
 }
 
