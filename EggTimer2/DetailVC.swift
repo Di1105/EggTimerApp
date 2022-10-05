de42f0a930bcf835e991fa5878805a8db9b694f2
mfsaglam <mfsaglam@yandex.com>
2022-10-05 09:46:58 +0300

    add timerButton to detailVC

--- EggTimer2/DetailVC.swift
+++ EggTimer2/DetailVC.swift
@@ -11,20 +11,31 @@ class DetailVC: UIViewController {
 
     var timeLabel = TimeLabel()
     var eggImage = UIImageView()
+    var timerButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+    }
     
+    @objc func timerButtonTapped() {
+        //start or pause timer here
     }
     
     private func configureUI(){
         view.addSubview(timeLabel)
         view.addSubview(eggImage)
+        view.addSubview(timerButton)
         
-        eggImage.image = UIImage(named: "sample")
+        eggImage.image = UIImage(named: "hard")
         eggImage.contentMode = .scaleAspectFit
         eggImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        timerButton.layer.cornerRadius = 10
+        timerButton.addTarget(self, action: #selector(timerButtonTapped), for: .touchUpInside)
+        timerButton.backgroundColor = UIColor(named: "buttonColor")
+        timerButton.setTitle("Pause", for: .normal)
+        timerButton.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             timeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
@@ -35,8 +46,12 @@ class DetailVC: UIViewController {
             eggImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
             eggImage.bottomAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: -160),
             eggImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
-            eggImage.heightAnchor.constraint(equalToConstant: 250)
+            eggImage.heightAnchor.constraint(equalToConstant: 250),
             
+            timerButton.leadingAnchor.constraint(equalTo: timeLabel.leadingAnchor, constant: 18),
+            timerButton.trailingAnchor.constraint(equalTo: timeLabel.trailingAnchor, constant: -18),
+            timerButton.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: -30),
+            timerButton.heightAnchor.constraint(equalToConstant: 60)
         ])
         
     }
